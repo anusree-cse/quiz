4ff6cd79b13f55652fb7dc7679053cdcffcdf0b2
anusree-cse <105202426+anusree-cse@users.noreply.github.com>
2024-04-05 12:40:04 +0530

    initial commit

---
+++ js/questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Find missing letter 'APP_E'",
+    answer: "L",
+    options: [
+      "G",
+      "L",
+      "E",
+      "H"
+    ]
+  },
+    {
+    numb: 2,
+    question: "1 + 10 =",
+    answer: "11",
+    options: [
+      "9",
+      "11",
+      "12",
+      "8"
+    ]
+  },
+    {
+    numb: 3,
+    question: "what is our national fruit?",
+    answer: "mango",
+    options: [
+      "apple",
+      "banana",
+      "mango",
+      "strawberry"
+    ]
+  },
+    {
+    numb: 4,
+    question: "which animal has black and white stripes?",
+    answer: "zebra",
+    options: [
+      "zebra",
+      "lion",
+      "elephant",
+      "horse"
+    ]
+  },
+    {
+    numb: 5,
+    question: "5 * 5 = ",
+    answer: "25",
+    options: [
+      "25",
+      "20",
+      "30",
+      "10"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
